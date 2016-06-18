9b9765cc860407a5adc3f1b3709b668f45d272e3
yogesh <yogesh.s@tvsi.com>
2016-06-18 22:02:32 +0530

    Making Master

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -1,24 +1,24 @@
-angular.module('starter.controllers', [])
+angular.module('medical.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('medicineCtrl', function($scope, medicineService) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //
   //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  // });
 
-  $scope.chats = Chats.all();
+  $scope.medicines = medicineService.all();
   $scope.remove = function(chat) {
-    Chats.remove(chat);
+    medicineService.remove(chat);
   };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function($scope, $stateParams, medicineService) {
+  $scope.medicine = medicineService.get($stateParams.medicineId);
 })
 
 .controller('AccountCtrl', function($scope) {
